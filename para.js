055602544e69409a455d76b63d7e5e3401f1277f
PARVATHI166DEVI <66837819+PARVATHI166DEVI@users.noreply.github.com>
2021-11-21 17:03:09 +0530

    Add files via upload

---
+++ para.js
@@ -0,0 +1,14 @@
+function textcolour(event){
+    var a = event.target.value;
+    document.getElementById("p1").style.color = a;
+}
+
+function textsize(event){
+    var b = event.target.value;
+    document.getElementById("p1").style.fontSize = b;
+}
+
+function textstyle(event){
+    var c = event.target.value;
+    document.getElementById("p1").style.fontStyle = c;
+}
